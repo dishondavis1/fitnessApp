4116dd0ab60d1364a57577c391c71c1bed0433d0
dishondavis1 <dishon.davis@slalom.com>
2021-12-21 17:21:18 -0500

    commit

---
+++ app/src/main/java/com/fitness/app/FitnessAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppBackendApplication.class, args);
+	}
+
+}
